6ebc103741e1dff012d01939c63bc04130452f31
sump-exe <cassy.is.niceu@gmail.com>
2026-04-20 00:55:16 +0800

    finally using php correctly

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ require __DIR__ . '/food-ordering-system.html';
 // no html syntax necessary, as the HTML file is included above
 // php code here meant to connect to the database
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'food_ordering_system_database';
+$host = '127.0.0.1';
+$user = 'root';
+$pass = '';
+$db = 'food_ordering_db';
+
+// try lang ng condition para ma-check kung successful ang connection
+$conn = new mysqli($host, $user, $pass);
+if (!$conn) {
+    echo "Connection failed: " . mysqli_connect_error();
+}
