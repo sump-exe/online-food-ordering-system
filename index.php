3887393f02dd03eeeacf2527ea7f4d5a018229b6
sump-exe <cassy.is.niceu@gmail.com>
2026-04-20 00:36:04 +0800

    basic linking to a database

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 require __DIR__ . '/food-ordering-system.html';
+
+// no html syntax necessary, as the HTML file is included above
+// php code here meant to connect to the database
+
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'food_ordering_system_database';
